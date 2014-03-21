5970a92c9651976051e7b7434d6e346ad5873016
Pat George <pat.george@hungrymachine.com>
2014-03-21 15:39:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = 'd487b62df45dca66bc76ec3ccab8571a4bd589edcfb3eaa892cd1430dbca18fe9bfafb95995d707a91f981230ddd38e0feb6e75cfe8138300ecf02ecf6a6a331'
